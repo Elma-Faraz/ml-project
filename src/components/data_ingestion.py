6c6232ab9d7c8da425e743e88fecdc0bd44b6be8
Elma Faraz <elma.faraz01@gmail.com>
2025-02-11 11:56:41 +0530

    model trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig():
@@ -22,6 +24,8 @@ class DataIngestion():
         try:
             df = pd.read_csv("notebook\data\stud.csv")
             logging.info("dataset read successfully")
+            
+            
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
@@ -45,5 +49,12 @@ class DataIngestion():
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,tes_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.inititate_data_transformation(train_data,tes_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
         
---
+++ src/components/data_transformation.py
@@ -0,0 +1,287 @@
+# from src.components.data_ingestion import DataIngestion
+# from src.exception import CustomException
+# from src.logger import logging
+# from sklearn.preprocessing import OneHotEncoder, StandardScaler
+# from sklearn.compose import ColumnTransformer
+# import pandas as pd
+# import numpy as np
+# import sys
+# import os
+# # Modelling
+# from sklearn.metrics import mean_squared_error, r2_score
+# from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.tree import DecisionTreeRegressor
+# from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+# from sklearn.svm import SVR
+# from sklearn.linear_model import LinearRegression, Ridge,Lasso
+# from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+# from sklearn.model_selection import RandomizedSearchCV
+# #from catboost import CatBoostRegressor
+# from xgboost import XGBRegressor
+# import warnings
+
+# def training_data_preprocessing(train_data_path):
+#     logging.info("reading train data")
+#     df = pd.read_csv(train_data_path)
+#     logging.info(df.head(5))
+    
+#     logging.info("taking features from data")
+#     X_train = df.drop(columns=['math_score'], axis=1)
+#     logging.info(f"features of training data {X_train.head(5)}")
+    
+#     y_train = df['math_score']
+#     logging.info(f"target of training data {y_train.head(5)}")
+    
+#     logging.info("splitting features into numerical and categorical features and preprocessing them")
+#     num_features = X_train.select_dtypes(exclude='object').columns
+#     cat_features = X_train.select_dtypes(include='object').columns
+    
+#     numeric_transformer = StandardScaler()
+#     cat_transformer = OneHotEncoder()
+    
+#     preprocessor = ColumnTransformer(
+#         [
+#             ("OneHotEncoder", cat_transformer, cat_features),
+#             ("StandardScaler", numeric_transformer, num_features)
+#         ]
+#     )
+    
+#     logging.info("fetures preprocessed successfully")
+    
+#     X_train = preprocessor.fit_transform(X_train)
+#     logging.info(f"shape of features after preprocessing {X_train.shape}")
+    
+#     return X_train,y_train
+
+# def preprocessing_test_data(test_data_path):
+#     logging.info("reading test data")
+#     df = pd.read_csv(test_data_path)
+#     logging.info(df.head(5))
+    
+#     logging.info("taking features from data")
+#     X_test = df.drop(columns=['math_score'], axis=1)
+#     logging.info(f"features of training data {X_test.head(5)}")
+    
+#     y_test = df['math_score']
+#     logging.info(f"target of training data {y_test.head(5)}")
+    
+#     logging.info("splitting features into numerical and categorical features and preprocessing them")
+#     num_features = X_test.select_dtypes(exclude='object').columns
+#     cat_features = X_test.select_dtypes(include='object').columns
+    
+#     numeric_transformer = StandardScaler()
+#     cat_transformer = OneHotEncoder()
+    
+#     preprocessor = ColumnTransformer(
+#         [
+#             ("OneHotEncoder", cat_transformer, cat_features),
+#             ("StandardScaler", numeric_transformer, num_features)
+#         ]
+#     )
+    
+#     logging.info("fetures preprocessed successfully")
+    
+#     X_test = preprocessor.transform(X_test)
+#     logging.info(f"shape of features after preprocessing {X_test.shape}")
+    
+#     return X_test,y_test
+    
+
+# def evaluate_model(true, predicted):
+#     mae = mean_absolute_error(true, predicted)
+#     mse = mean_squared_error(true, predicted)
+#     rmse = np.sqrt(mean_squared_error(true, predicted))
+#     r2_square = r2_score(true, predicted)
+#     return mae, rmse, r2_square
+
+# def data_training(train_data_path, test_data_path, models):
+#     logging.info("training started")
+#     try:
+#         X_train, y_train = training_data_preprocessing(train_data_path)
+#         X_test, y_test = preprocessing_test_data(test_data_path)
+        
+
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+#             model.fit(X_train, y_train) # Train model
+
+#             # Make predictions
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+            
+#             # Evaluate Train and Test dataset
+#             result_train = evaluate_model(y_train, y_train_pred)
+
+#             result_test = evaluate_model(y_test, y_test_pred)
+            
+#             return result_train, result_test
+        
+#     except Exception as e:
+#         raise CustomException(e,sys)
+    
+# if __name__ == "__main__":
+#     dataingestion_obj = DataIngestion()
+
+#     train_data_path, test_data_path = dataingestion_obj.initiate_data_ingestion()
+#     print("train data path",train_data_path)
+#     print("127 test data path ", test_data_path)
+
+#     models = {
+#             "Linear Regression": LinearRegression(),
+#             "Lasso": Lasso(),
+#             "Ridge": Ridge(),
+#             "K-Neighbors Regressor": KNeighborsRegressor(),
+#             "Decision Tree": DecisionTreeRegressor(),
+#             "Random Forest Regressor": RandomForestRegressor(),
+#             "XGBRegressor": XGBRegressor(), 
+#             # "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+#             "AdaBoost Regressor": AdaBoostRegressor()
+#             }
+#     model_list = []
+#     r2_list =[]
+#     training_result, test_result = data_training(train_data_path, test_data_path, models)
+#     model_train_mae, model_train_rmse, model_train_r2 = training_result
+#     model_test_mae, model_test_rmse, model_test_r2 = test_result
+    
+#     for i in range(len(list(models))):
+#         print(list(models.keys())[i])
+#         model_list.append(list(models.keys())[i])
+        
+#         print('Model performance for Training set')
+#         print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+#         print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+#         print("- R2 Score: {:.4f}".format(model_train_r2))
+
+#         print('----------------------------------')
+        
+#         print('Model performance for Test set')
+#         print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+#         print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+#         print("- R2 Score: {:.4f}".format(model_test_r2))
+#         r2_list.append(model_test_r2)
+        
+#         print('='*35)
+#         print('\n')
+    
+import sys
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path =  os.path.join("artifacts", "preproceeor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        this method is responsible for data transormation based on the different types of data.
+        '''
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course"
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("scaler",StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+            )
+
+            logging.info("Numerical columns standar scaling completed")
+            logging.info("Categorical columns encoding completed")
+
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipeline",cat_pipeline, categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def inititate_data_transformation(self,train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            print("****235",train_df.columns)
+            print("*****236",train_df.shape)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("read train and test data completed")
+
+            logging.info("obtaining processing oject")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score","reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            targte_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            targte_feature_test_df = test_df[target_column_name]
+
+            logging.info("applying preprocessing object on training dataset and testing dataset")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr,np.array(targte_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr,np.array(targte_feature_test_df)]
+
+            logging.info("saved processing object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+                )
+
+            return (
+            train_arr,
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file_path, 
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+
+
+
+
+    
+    
+    
+    
